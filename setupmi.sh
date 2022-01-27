88e0d5296131630c92bd3dba267cbb82ae129137
NillyTheL0L <97253814+NillyTheL0L@users.noreply.github.com>
2022-01-27 10:02:05 -0300

    setup file

---
+++ setupmi.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+wget https://raw.githubusercontent.com/NillyTheL0L/multiinstall/main/multiinstall.sh && sudo mv multiinstall.sh /usr/bin/multiinstall && chmod +x /usr/bin/multiinstall && echo INSTALLED WITH SUCESS
