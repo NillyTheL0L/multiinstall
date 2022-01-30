cf9456f867b1118e7d5ed177a5183202de9fbf13
NillyTheL0L <97253814+NillyTheL0L@users.noreply.github.com>
2022-01-30 14:28:10 -0300

    Added void linux support

--- multiinstall.sh
+++ multiinstall.sh
@@ -7,10 +7,11 @@ usage: ${0##*/} [flags]
 
   Options:
 
-    --ubuntu          Ubuntu Based.
-    --fedora             Fedora Based.
-    --arch             Arch Based.
-    --opensuse             openSUSE Based.
+	--ubuntu          Ubuntu Based.
+	--fedora             Fedora Based.
+	--arch             Arch Based.
+	--opensuse             openSUSE Based.
+	--void			VoidLinux Based.
 
 * MultiInstall NillyTheL0L 2022-2022
 
@@ -42,9 +43,20 @@ echo Hello $USERNAME just accept all and wait && sudo pacman -S ark micro vim gi
 opensuse(){
 
 
-echo Hello $USERNAME just accept all and wait && sudo zypper in ark vim gimp vlc blender thunderbird flatpak inkscape gnome-terminal ktorrent gedit figlet nano snapd git curl wget &&  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && flatpak install flathub com.valvesoftware.Steam && sudo snap install code --classic && sudo snap install authy && figlet installed && figlet Reboot Now
+echo Hello $USERNAME just accept all and wait && sudo zypper install ark vim gimp vlc blender thunderbird flatpak inkscape gnome-terminal ktorrent gedit figlet nano snapd git curl wget &&  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && flatpak install flathub com.valvesoftware.Steam && sudo snap install code --classic && sudo snap install authy && figlet installed && figlet Reboot Now
 	
 }
+
+
+void(){
+
+
+echo Hello $USERNAME just accept all and wait && sudo xbps-install  ark vim gimp vlc blender thunderbird flatpak inkscape gnome-terminal ktorrent gedit figlet nano git curl wget &&  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo && flatpak install flathub com.valvesoftware.Steam  && figlet installed && figlet Reboot Now
+	
+}
+
+
+
 
 cat <<EOF
 --------------MULTIINSTALL SCRIPT BY NILLYTHEL0L--------------
@@ -59,6 +71,7 @@ while [[ "$1" ]]; do
             --fedora|-f) fedora;;
             --arch|-a) arch;;
             --opensuse|-o) opensuse;;
+            --void|-v) void;;
         esac
         shift
     } || {
