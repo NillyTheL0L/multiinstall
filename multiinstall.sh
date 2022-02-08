134082d7b311eb7f747ff4f8986518c41489177c
NillyTheL0L <97253814+NillyTheL0L@users.noreply.github.com>
2022-02-08 20:05:05 -0300

    new ascii at accept all

--- multiinstall.sh
+++ multiinstall.sh
@@ -77,7 +77,13 @@ echo Hello $USERNAME just accept all and wait && pkg install ark gimp-app vlc bl
 
 
 cat <<EOF
---------------MULTIINSTALL SCRIPT BY NILLYTHEL0L--------------
+ __  __       _ _   _ ___           _        _ _
+ |  \/  |_   _| | |_(_)_ _|_ __  ___| |_ __ _| | |
+ | |\/| | | | | | __| || || '_ \/ __| __/ _` | | |
+ | |  | | |_| | | |_| || || | | \__ \ || (_| | | |
+ |_|  |_|\__,_|_|\__|_|___|_| |_|___/\__\__,_|_|_|
+
+                                  NillyTheL0L - 2022-2022                
 EOF
 
 while [[ "$1" ]]; do
